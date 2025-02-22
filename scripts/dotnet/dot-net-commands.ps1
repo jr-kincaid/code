84b73865bfa4ea1003309f1984f4634fbbc1ff3b
Joshua <114257070+jr-kincaid@users.noreply.github.com>
2025-02-22 15:54:17 -0500

    Starting to Read https://learning.oreilly.com/library/view/c-12-and/9781837635870/Text/Chapter_01.xhtml#_idParaDest-54
    Starting to Read https://learning.oreilly.com/library/view/c-12-and/9781837635870/Text/Chapter_01.xhtml#_idParaDest-54

--- scripts/dotnet/dot-net-commands.ps1
+++ scripts/dotnet/dot-net-commands.ps1
@@ -1,3 +1,8 @@
 dotnet --list-sdks #lists installed sdks
 dotnet --list-runtimes #lists installed runtimes
 dotnet --info #list some dotnet and enviorment info
+dotnet new sln --name SolutionName #create a new solution named SolutionName
+dotnet new console --output ConsoleProject #create new project named ConsoleProject
+dotnet sln add ConsoleProject #add a project named ConsoleProject to the solution
+code . #Open current folder in VS Code
+#Next Step Finish creating projects with https://learning.oreilly.com/library/view/c-12-and/9781837635870/Text/Chapter_01.xhtml#_idParaDest-54
