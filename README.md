16f4393f3efb1d2b1f4442a2f1593b959fae5a87
Joshua <114257070+jr-kincaid@users.noreply.github.com>
2025-09-08 19:21:01 -0400

    Nuke

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Purpose
+
 I use this repo to store code I write during my personal professional development time.
--- scripts/vs-code/vs-code-c-sharp-env-create.ps1
+++
@@ -1,5 +0,0 @@
-code --install-extension ms-dotnettools.csdevkit
-code --install-extension ms-dotnettools.dotnet-interactive-vscode
-code --install-extension tintoy.msbuild-project-tools
-code --install-extension humao.rest-client
-code --install-extension icsharpcode.ilspy-vscode
