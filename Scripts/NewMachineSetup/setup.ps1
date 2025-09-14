5b678a4edd67099e3b78f257bdc92d690d640b3f
J.R. Kincaid <devkincaid@icloud.com>
2025-09-14 16:16:05 -0400

    Komorebi

---
+++ Scripts/NewMachineSetup/setup.ps1
@@ -0,0 +1,7 @@
+winget install vscode chrome Mozilla.Firefox 
+winget install LGUG2Z.komorebi 
+winget install LGUG2Z.whkd  
+winget install opengl
+komorebic quickstart
+komorebic start --whkd --bar
+# https://lgug2z.github.io/komorebi/usage/moving-windows.html
